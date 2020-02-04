1bcc3f708eed1a136113a4ad3d0eef8e30473681
Ivan Smirnov <the.ivan.smirnov@gmail.com>
2020-02-04 09:21:23 +0400

    Updates the site

---
+++ precache-manifest.d156ca97509b2247c39a7f46f42c727d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36f74598f13740c129b7ba7e680276f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa5a0a39db936b2c0d1",
+    "url": "/static/css/main.29d75181.chunk.css"
+  },
+  {
+    "revision": "01df72585dbade16e043",
+    "url": "/static/js/2.ba5cc733.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.ba5cc733.chunk.js.LICENSE"
+  },
+  {
+    "revision": "faa5a0a39db936b2c0d1",
+    "url": "/static/js/main.f95f8352.chunk.js"
+  },
+  {
+    "revision": "454f9ead867b4c92973e",
+    "url": "/static/js/runtime-main.815eef4e.js"
+  },
+  {
+    "revision": "4c0c46c5025725bf1197772cd6ff87f0",
+    "url": "/static/media/instagram-icon.4c0c46c5.svg"
+  },
+  {
+    "revision": "014a33bbb2c8818ae7d70523951afe7b",
+    "url": "/static/media/manrope-extrabold.014a33bb.woff"
+  },
+  {
+    "revision": "6fdca7c1b7bfed0cdd1d614e5bf777af",
+    "url": "/static/media/manrope-extrabold.6fdca7c1.woff2"
+  },
+  {
+    "revision": "5db703858cf666908865cf6ecf8a3af4",
+    "url": "/static/media/manrope-medium.5db70385.woff2"
+  },
+  {
+    "revision": "e59358f6346cdb5ba686235855a5eb5c",
+    "url": "/static/media/manrope-medium.e59358f6.woff"
+  },
+  {
+    "revision": "871132a5c6001e1adb29b996afc0aab7",
+    "url": "/static/media/manrope-regular.871132a5.woff"
+  },
+  {
+    "revision": "8ae23f32b65044b6eb29333438021730",
+    "url": "/static/media/manrope-regular.8ae23f32.woff2"
+  },
+  {
+    "revision": "254387411c0f00ed00fdd97b79168773",
+    "url": "/static/media/manrope-semibold.25438741.woff2"
+  },
+  {
+    "revision": "d31d52534b371c2f05fec56e8085c485",
+    "url": "/static/media/manrope-semibold.d31d5253.woff"
+  },
+  {
+    "revision": "5004fb413974b1cd0c12a23f38969db4",
+    "url": "/static/media/my-photo.5004fb41.png"
+  },
+  {
+    "revision": "5a49ab2e1d59b0b787e5719e4bf77df0",
+    "url": "/static/media/telegram-icon.5a49ab2e.svg"
+  },
+  {
+    "revision": "5be43f483c456b1ecf75c01b2c4d3ff7",
+    "url": "/static/media/vk-icon.5be43f48.svg"
+  }
+]);
