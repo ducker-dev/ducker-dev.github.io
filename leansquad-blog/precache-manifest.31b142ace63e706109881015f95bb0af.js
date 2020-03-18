1eb206d8265e8768b07dc3a195261a642dea8931
Ivan Smirnov <the.ivan.smirnov@gmail.com>
2020-03-18 12:25:37 +0400

    Add site "leansquad-blog"

---
+++ leansquad-blog/precache-manifest.31b142ace63e706109881015f95bb0af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb6441313f56351042cf65f2a9453a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "860f27e61d8f7c12d517",
+    "url": "/static/css/main.53a4c4b8.chunk.css"
+  },
+  {
+    "revision": "684f228224b7853a20dc",
+    "url": "/static/js/2.8ab74a97.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.8ab74a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "860f27e61d8f7c12d517",
+    "url": "/static/js/main.0425dc76.chunk.js"
+  },
+  {
+    "revision": "fca99288d709c26771f4",
+    "url": "/static/js/runtime-main.01e821d5.js"
+  }
+]);
